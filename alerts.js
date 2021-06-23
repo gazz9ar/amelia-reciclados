7f69762fc42e7aa8c5edceb2cbd4fcbffe7fd0a1
GS <gazz9ar@gmail.com>
2021-06-23 18:38:42 -0300

    cambios al 23/6

---
+++ alerts.js
@@ -0,0 +1,50 @@
+
+var form = document.getElementById("form-contacto");
+
+var inputs = document.getElementsByClassName("form-control");
+    
+async function handleSubmit(event) {
+  
+      event.preventDefault();
+      // var status = document.getElementById("my-form-status");
+      // console.log(inputs.email.value,inputs.name.value,inputs.mensaje.value);
+      if (inputs.email.value == "" || inputs.name.value == "" || inputs.mensaje.value == "")
+       {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error!',
+          text: 'Complete todo los campos!'
+        })
+      }
+      else  
+      {
+          var data = new FormData(event.target);
+        // console.log(event.target);
+        fetch(event.target.action, {
+          method: form.method,
+          body: data,
+          headers: {
+              'Accept': 'application/json'
+          }
+        }).then(response => {
+          Swal.fire(
+            'Enviado!',
+            'Muchas gracias por tu mensaje, pronto nos contactaremos contigo.',
+            'success'
+          )
+          form.reset()
+        }).catch(error => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Algo ha salido mal! Intentalo de nuevo'
+          })
+        });
+      }
+      
+
+
+    }
+
+
+    form.addEventListener("submit", handleSubmit)
