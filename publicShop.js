fb4d95fc66a7cbb89143936207a0c17fcc62767f
GS <gazz9ar@gmail.com>
2021-07-29 10:20:12 -0300

    Visualizacion de productos en tienda correcto

--- publicShop.js
+++ publicShop.js
@@ -18,13 +18,25 @@ const col3 = document.getElementById("col-3");
 let products = [];
 
 
-window.addEventListener('DOMContentLoaded', (e) => {
+function verProducto(uid){
+
+
+
+
+
+}
+
+window.addEventListener('DOMContentLoaded', async (e) => {
+
+    
 
     onGetCollection('products',( querySnapshot => {
       
       products = [];
   
       col1.innerHTML = '';
+      col2.innerHTML = '';
+      col3.innerHTML = '';
 
 
       
@@ -53,13 +65,158 @@ window.addEventListener('DOMContentLoaded', (e) => {
         }
       );
 
-        console.log(products);
+        
 
         // PRIMER ARRAY CON EL QUE DEBO RENDERIZXAR COL-1
-        let arrayCol1 = products.slice(0, Math.ceil(products.length / 3) );
+        let arrayCol1 = [];
+        let arrayCol2 = [];
+        let arrayCol3 = [];
+
+
+        // ============================================================================================================================================
+        // MAQUETADO DISEÑADO EN 3 COLUMNAS COMO INSTAGRAM, NO IMPORTA LA CANT  DE PRODUCTOS, SE DEBEN IR CARGANDO DE IZQ  A DERECHA EN LAS 3 COLUMNAS
+        // ============================================================================================================================================
+
+        // Si la cant de productos es divisible por 3
+        if (products.length % 3 == 0) {
+
+            // lo divido en partes iguales
+           arrayCol1 = products.slice(0, Math.ceil(products.length / 3) );
         
-        console.log(arrayCol1);      
+           arrayCol2 = products.slice(Math.ceil(products.length / 3), ( products.length - Math.ceil(products.length / 3 )));
+
+           arrayCol3 = products.slice(( products.length - Math.ceil(products.length / 3 )), products.length);
+        } else {
+
+          // 1 sobra = 1er columna ------------- 2 sobra = 1ra y 2da columna
+           arrayCol1 = products.slice(0, Math.ceil(products.length / 3) );
         
+           arrayCol2 = products.slice(Math.ceil(products.length / 3), ( products.length - Math.ceil(products.length / 3 )) + 1);
+          
+           arrayCol3 = products.slice(( products.length - Math.ceil(products.length / 3 ) + 1), products.length);
+        }
+        
+        for (const product of arrayCol1) {
+          
+          let docRef = db.collection('products').doc(product.uid);
+         
+          docRef.get().then((doc)=>{
+            let productDocument = doc.data();
+            if (doc.exists) {
+              let storageRef  = firebase.storage().ref(); 
+              storageRef.child('shopImages/' + productDocument.imgUrl).getDownloadURL()
+              .then(function(url){
+                col1.innerHTML += `
+            
+                  <div class="item-wrapper" id="product-uid-${product.uid}">
+                          <a href="#" class="d-block position-relative product-link" type="button"
+                              data-bs-toggle="modal" data-bs-target="#modalProximamente">
+                              <img src="${url}" alt="Mueble Barniz" class="img-thumbnail">
+                              <!-- OCULTO -->
+                              <div
+                                  class="position-absolute text-center w-75  start-50 agregar translate-middle d-none m-auto">
+                                  <p>Agregar al Carrito</p>
+                              </div>
+                          </a>
+
+                          <h3 class="fs-5 product-title mb-1 mt-2"> ${product.nombre}</h3>
+                          <p class="fs-6 product-text mt-0 mb-1">${product.description}</p>
+                          <p class="fs-6 product-text">$${product.precio}</p>
+
+                      </div>
+                  `;
+                  const itemX = document.getElementById(`product-uid-${product.uid}`);
+
+                  itemX.setAttribute('onclick',`verProducto('${product.uid}')`);
+
+              })
+
+
+            }
+          });
+          
+            
+           
+        }
+        for (const product of arrayCol2) {
+          
+          let docRef = db.collection('products').doc(product.uid);
+         
+          docRef.get().then((doc)=>{
+            let productDocument = doc.data();
+            if (doc.exists) {
+              let storageRef  = firebase.storage().ref(); 
+              storageRef.child('shopImages/' + productDocument.imgUrl).getDownloadURL()
+              .then(function(url){
+                col2.innerHTML += `
+            
+                  <div class="item-wrapper" id="product-uid-${product.uid}">
+                          <a href="#" class="d-block position-relative product-link" type="button"
+                              data-bs-toggle="modal" data-bs-target="#modalProximamente">
+                              <img src="${url}" alt="Mueble Barniz" class="img-thumbnail">
+                              <!-- OCULTO -->
+                              <div
+                                  class="position-absolute text-center w-75  start-50 agregar translate-middle d-none m-auto">
+                                  <p>Agregar al Carrito</p>
+                              </div>
+                          </a>
+
+                          <h3 class="fs-5 product-title mb-1 mt-2"> ${product.nombre}</h3>
+                          <p class="fs-6 product-text mt-0 mb-1">${product.description}</p>
+                          <p class="fs-6 product-text">$${product.precio}</p>
+
+                      </div>
+                  `;
+                  const itemX = document.getElementById(`product-uid-${product.uid}`);
+
+                  itemX.setAttribute('onclick',`verProducto('${product.uid}')`);
+
+              })
+
+
+            }
+          });
+      }
+        for (const product of arrayCol3) {
+            
+          let docRef = db.collection('products').doc(product.uid);
+         
+          docRef.get().then((doc)=>{
+            let productDocument = doc.data();
+            if (doc.exists) {
+              let storageRef  = firebase.storage().ref(); 
+              storageRef.child('shopImages/' + productDocument.imgUrl).getDownloadURL()
+              .then(function(url){
+                col3.innerHTML += `
+            
+                  <div class="item-wrapper" id="product-uid-${product.uid}">
+                          <a href="#" class="d-block position-relative product-link" type="button"
+                              data-bs-toggle="modal" data-bs-target="#modalProximamente">
+                              <img src="${url}" alt="Mueble Barniz" class="img-thumbnail">
+                              <!-- OCULTO -->
+                              <div
+                                  class="position-absolute text-center w-75  start-50 agregar translate-middle d-none m-auto">
+                                  <p>Agregar al Carrito</p>
+                              </div>
+                          </a>
+
+                          <h3 class="fs-5 product-title mb-1 mt-2"> ${product.nombre}</h3>
+                          <p class="fs-6 product-text mt-0 mb-1">${product.description}</p>
+                          <p class="fs-6 product-text">$${product.precio}</p>
+
+                      </div>
+                  `;
+                  const itemX = document.getElementById(`product-uid-${product.uid}`);
+
+                  itemX.setAttribute('onclick',`verProducto('${product.uid}')`);
+
+              })
+
+
+            }
+          });
+      }
+
      
     }))
     
